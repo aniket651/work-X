02e477cf305d9c01122b7a8b3c81095dfce6223f
aniket651 <73066452+aniket651@users.noreply.github.com>
2023-09-07 19:54:19 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const path = require('path');
 
 
-app.use(cors());//do remove it or configure it before pushing to production. it is here to allow requests from all sources
+// app.use(cors());//do remove it or configure it before pushing to production. it is here to allow requests from all sources
 
 app.use(express.json());
 app.use('/',indexRoutes);
@@ -22,11 +22,11 @@ app.use('/user',userRoutes);
 
 
 
-app.use(express.static(path.join(__dirname,'./frontend/build')));
+// app.use(express.static(path.join(__dirname,'./frontend/build')));
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./frontend/build/index.html'));
-})
+// app.get('*',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'./frontend/build/index.html'));
+// })
 
 
 
