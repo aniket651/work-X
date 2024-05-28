1f54f6dcedf61f7eeff1cae0704730c6920e49f8
aniket <aniketpandey651@gmail.com>
2024-05-28 21:34:11 +0530

    removing an error

--- frontend/src/auth/Auth.js
+++ frontend/src/auth/Auth.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import AuthFooter from "./footer/AuthFooter";
 import AuthNavbar from "./navbar/AuthNavbar";
 const Auth = () => {
     return (
