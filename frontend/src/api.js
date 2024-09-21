66915dbd7ca770924c1b29343fb04f81aeb89f54
aniket <aniketpandey651@gmail.com>
2024-09-21 20:39:08 +0530

    chanching backend server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const api = axios.create({
 
   // baseURL: 'http://localhost:3003',
-  baseURL: 'https://workx-backend.onrender.com'//do remember to change the url in auth/login as well
+  // baseURL: 'https://workx-backend.onrender.com'//do remember to change the url in auth/login as well
+  baseURL: 'https://work-x-backend.vercel.app/'
 });
 
 
